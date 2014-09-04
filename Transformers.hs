a9d3340f25669fb199a555beda0349f0fd75724f
Brian Schroeder <bts@gmail.com>
2014-09-03 23:44:11 -0400

    Add base monadic evaluator

--- Transformers.hs
+++ Transformers.hs
@@ -1,12 +1,12 @@
 module Transformers where
 
---import Control.Monad.Identity
+import Data.Maybe
+import qualified Data.Map as Map
+import Control.Monad.Identity
 --import Control.Monad.Except
 --import Control.Monad.Reader
 --import Control.Monad.State
 --import Control.Monad.Writer
-import Data.Maybe
-import qualified Data.Map as Map
 
 type Name = String
 
@@ -24,7 +24,7 @@ data Value = IntVal Integer
 type Env = Map.Map Name Value
 
 -- non-monadic "reference impl" evaluator:
--- intentionally superfluous to compare with forthcoming monadic version below
+-- intentionally superfluous for comparison with forthcoming monadic version
 eval0 :: Env -> Exp -> Value
 eval0 _ (Lit i) = IntVal i
 eval0 env (Var n) = fromJust (Map.lookup n env)
@@ -33,10 +33,31 @@ eval0 env (Plus e1 e2) = let IntVal i1 = eval0 env e1
                          in IntVal (i1 + i2)
 eval0 env (Abs n e) = FunVal env n e
 eval0 env (App e1 e2) =
-  let val1 = eval0 env e1
-      val2 = eval0 env e2
-  in case val1 of
-      FunVal env' n body -> eval0 (Map.insert n val2 env') body
+  let v1 = eval0 env e1
+      v2 = eval0 env e2
+  in case v1 of
+      FunVal env' n body -> eval0 (Map.insert n v2 env') body
 
 -- exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
 -- eval0 Map.empty exampleExp
+
+type Eval1 a = Identity a
+
+runEval1 :: Eval1 a -> a
+runEval1 = runIdentity
+
+eval1 :: Env -> Exp -> Eval1 Value
+eval1 _ (Lit i) = return $ IntVal i
+eval1 env (Var n) = return $ fromJust (Map.lookup n env)
+eval1 env (Plus e1 e2) = do IntVal i1 <- eval1 env e1
+                            IntVal i2 <- eval1 env e2
+                            return $ IntVal (i1 + i2)
+eval1 env (Abs n e) = return $ FunVal env n e
+eval1 env (App e1 e2) =
+  do v1 <- eval1 env e1
+     v2 <- eval1 env e2
+     case v1 of
+      FunVal env' n body -> eval1 (Map.insert n v2 env') body
+
+-- exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
+-- runEval1 $ eval1 Map.empty exampleExp
